f4b70fa7d35be337f76a46da4e35bbbce684b217
kristina84 <74793682+kristina84@users.noreply.github.com>
2020-12-05 01:01:29 +0300

    Init

---
+++ src/kristina.java
@@ -0,0 +1,15 @@
+public class kristina {
+    public static void main(String[]args){
+        String name = "kristina";
+        char initial = 'a';
+        int age = 32;
+        Boolean isRaining = false;
+        double distance =1050.4;
+
+      System.out.println(name);
+       System.out.println(initial);
+       System.out.println(age);
+       System.out.println(isRaining);
+       System.out.println(distance);
+    }
+}
